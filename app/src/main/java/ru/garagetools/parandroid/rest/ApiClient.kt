ef6cffd4be304d7b2db635c7de57a2d93f509674
Сергей Шевелев <shevelev.s@garagetools.ru>
2020-03-23 13:22:44 +0300

    add basic auth for api

--- app/src/main/java/ru/garagetools/parandroid/rest/ApiClient.kt
+++ app/src/main/java/ru/garagetools/parandroid/rest/ApiClient.kt
@@ -1,6 +1,7 @@
 package ru.garagetools.parandroid.rest
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,18 +9,24 @@ import retrofit2.converter.gson.GsonConverterFactory
 class ApiClient {
 
     companion object {
-        val baseURL : String = "http://garage-pc127:8080/"
+        val baseURL : String = "http://garage-pc127:8081/"
+
         var retrofit : Retrofit? = null
 
         var gson = GsonBuilder()
             .setLenient()
             .create()
 
+        val client2 =  OkHttpClient.Builder()
+            .addInterceptor(BasicAuthInterceptor("api", "testpass"))
+            .build()
+
         val client : Retrofit
             get(){
                 if (retrofit == null) {
                     retrofit = Retrofit.Builder()
                         .baseUrl(baseURL)
+                        .client(client2)
                         .addConverterFactory(GsonConverterFactory.create(gson))
                         .build()
                 }
